4fb24c3f3daecf61ce003477ce26666481a46fb6
Vishal22598 <vishalgupta22598@gmail.com>
2026-05-05 23:58:35 +0530

    no changes

--- C++/demo.cpp
+++ C++/demo.cpp
@@ -1,45 +1,14 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<map>
 using namespace std;
 
-int brute(vector<int> arr){
-    sort(arr.begin(), arr.end());
-    for(int i: arr)
-        cout << i << ", ";
-    int maxLen = 1;
-    for(int i=0; i<arr.size(); i++){
-        int len = 1; 
-        for(int j=i+1; j<arr.size(); j++){
-            if(arr[j]-arr[j-1] == 1)
-                len++;
-        }
-        maxLen = len > maxLen ? len : maxLen;
-    }
-    return maxLen;
-}
-
-int better(vector<int> arr){
-    sort(arr.begin(), arr.end());
-    int maxLen = 1, len = 1; 
-    for(int i=1; i<arr.size(); i++){
-        if(arr[i]-arr[i-1] == 1)
-            len++;
-        else
-            len = 1;
-        maxLen = len>maxLen ? len : maxLen;
-    }
-    return maxLen;
-}
 
 int main(){
-    vector<int> arr = {100, 4, 200, 1, 3, 2};
-    // for(int i=0;i<n; i++){
-    //     cin >> arr[i];
-    // }
-
-    // cout << endl << "max: " << brute(arr) ;
-    cout << "max: " << better(arr) ;
-
+    int n, m;
+    n=3;
+    m=3;
+    int arr[n][m] = {};
     return 0;
 }
