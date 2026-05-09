64d989ed27a84162f81e6b817df7037bf815d9c7
Vishal22598 <vishalgupta22598@gmail.com>
2026-05-10 01:04:26 +0530

    lower bound and uper bound program based on binary search done

--- C++/BinarySearch/BinarySearchOn1Darray/lowerBound.cpp
+++ C++/BinarySearch/BinarySearchOn1Darray/lowerBound.cpp
@@ -11,18 +11,18 @@ int bs(vector<int> arr, int key){
             l = mid+1;
         else{
             int i=mid;
-            while(i<=arr.size()-1 && arr[i] == arr[mid]){
-                i++;
+            while(i>=0 && arr[i] == arr[mid]){
+                i--;
             }
-            return i-1;
+            return i+1;
         }
     }
-    return 0;
+    return arr.size()-1;
 }
 
 int binarySearch(vector<int> arr, int l, int r, int key){
     if(l>r)
-        return 0;
+        return r;
 
     int mid= l+ (r-l)/2;
     if(key < arr[mid])
@@ -31,20 +31,20 @@ int binarySearch(vector<int> arr, int l, int r, int key){
         return binarySearch(arr, mid+1, r, key);
     else{
             int i=mid;
-            while(i<=arr.size()-1 && arr[i] == arr[mid]){
-                i++;
+            while(i>=0 && arr[i] == arr[mid]){
+                i--;
             }
-            return i-1;
+            return i+1;
         }
     return -1;
 }
 int main(){
     
     // vector<int> arr = {3, 4, 6, 7, 9, 12, 16, 17};
-    vector<int> arr = {1, 2, 2, 2, 2, 2, 4};
-    int key=2;
-    // int ind = binarySearch(arr, 0, arr.size()-1, key);
-    int ind = bs(arr, key);
+    vector<int> arr = {2, 2, 2, 4};
+    int key=21;
+    int ind = binarySearch(arr, 0, arr.size()-1, key);
+    // int ind = bs(arr, key);
     if(ind != -1)
         cout << "lower bound of element " << key << " is: " << ind;
     else
